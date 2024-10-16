37ce7938af4142d622f153b622195818206f5320
Yoonseop Kang <e0engoon@gmail.com>
2024-10-16 19:22:22 +0900

    Port some codes from other repo

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,8 @@ const unsigned SDA_PIN = 8;
 const unsigned SCL_PIN = 9;
 const unsigned MPU6050_I2C = 0x68;
 
-const unsigned AFS_SEL = 1; // +/- 2g, 16,384 steps/g
-const float STEPS_PER_G = 16384.0 / (2 << AFS_SEL);
-
-const unsigned FS_SEL = 0; // +/- 250 deg/s, 131 steps/(deg/s)
-const float STEPS_PER_DEG_PER_SECOND = 131.0 / (2 << FS_SEL);
+const unsigned AFS_SEL = 0;
+const unsigned FS_SEL = 3;
 
 MPU6050 mpu(MPU6050_I2C);
 
@@ -31,23 +28,23 @@ float gyro_unit;
 float accel_unit;
 float av_si[3];
 float aa_si[3];
+long last_ts;
 
 float angle_accel[3];
 float angle_gyro[3];
 
 // DRV8835 configurations
-const int PWM_FREQUENCY = 50000;
-const int PWM_RESOLUTION = 8;
+const int PWM_FREQUENCY = 5000;
+const int PWM_RESOLUTION = 12;
+const int PWM_MAX = (1 << PWM_RESOLUTION);
+
 const unsigned IN1EN_PIN = 3;
 const unsigned IN1PH_PIN = 2;
 const unsigned IN2EN_PIN = 1;
 const unsigned IN2PH_PIN = 0;
 
-int8_t left_speed = 0;
-int8_t right_speed = 0;
-
 void get_angle_from_accel(float aa_si[3], float *angle);
-void drive_motor(uint8_t channel, uint8_t pin, int8_t speed);
+void drive_motor(uint8_t channel, uint8_t pin, int16_t speed);
 
 void setup() {
   Serial.begin(9600);
@@ -82,44 +79,42 @@ void setup() {
   mpu.setDMPEnabled(true);
   packet_size = mpu.dmpGetFIFOPacketSize();
 
+  float int16_max = 1 << 15;
+
   int gyro_range = mpu.getFullScaleGyroRange();
-  gyro_unit = 0.0;
-  if (gyro_range == 0) {
-    gyro_unit = 250.0;
-  } else if (gyro_range == 1) {
-    gyro_unit = 500.0;
-  } else if (gyro_range == 2) {
-    gyro_unit = 1000.0;
-  } else if (gyro_range == 3) {
-    gyro_unit = 2000.0;
-  }
-  gyro_unit /= 16384.0;
+  gyro_unit = 250.0 * (1 << gyro_range) / int16_max;
 
   int accel_range = mpu.getFullScaleAccelRange();
-  accel_unit = 0.0;
-  if (accel_range == 0) {
-    accel_unit = 2.0;
-  } else if (accel_range == 1) {
-    accel_unit = 4.0;
-  } else if (accel_range == 2) {
-    accel_unit = 8.0;
-  } else if (accel_range == 3) {
-    accel_unit = 16.0;
-  }
-  accel_unit /= 16384.0;
-  accel_unit *= 9.8;
+  accel_unit = 9.8 * (2 << accel_range) / int16_max;
+
+  last_ts = micros();
 }
 
 void loop() {
+  float c_p = 10.0;
+  float c_i = 0.0;
+  float c_d = 0.0;
+  float error = 0.0;
+  float speed = 0.0;
+  long now_ts = last_ts;
+  float dt = 0.0;
+
+  float tau = 0.01;
+  float angle[3] = {0, 0, 0};
+
   if (!is_connected) {
     Serial.println("MPU-6050 not available");
   }
 
   if (mpu.dmpPacketAvailable()) {
+
     mpu.dmpGetCurrentFIFOPacket(packet);
 
     mpu.dmpGetGyro(&av, packet);
     mpu.dmpGetAccel(&aa, packet);
+
+    now_ts = micros();
+    dt = (now_ts - last_ts) / 1000000.0;
 
     // degree per sec
     av_si[0] = float(av.x) * gyro_unit;
@@ -133,21 +128,22 @@ void loop() {
     aa_si[2] = float(aa.z) * accel_unit;
     get_angle_from_accel(aa_si, angle_accel);
 
-    Serial.printf("v = (%f, %f, %f)\n", av_si[0], av_si[1], av_si[2]);
-    Serial.printf("a = (%f, %f, %f)\n", aa_si[0], aa_si[1], aa_si[2]);
-    Serial.printf("angle(radian) = (%f, %f, %f)\n", angle_accel[0],
-                  angle_accel[1], angle_accel[2]);
-    Serial.printf("angle(radian) = (%f, %f, %f)\n", angle_gyro[0],
-                  angle_gyro[1], angle_gyro[2]);
+    float accelRoll = atan2(aa_si[0], aa_si[2]) * (180 / M_PI);
+    float accelPitch = atan2(aa_si[1], aa_si[2]) * (180 / M_PI);
 
-    left_speed = int8_t(aa.x >> 6);
-    right_speed = -int8_t(aa.x >> 6);
-    Serial.printf("speed = (%d, %d)\n", left_speed, right_speed);
+    angle[0] = (tau) * (angle[0] + av_si[0] * dt) + (1.0 - tau) * accelPitch;
+    angle[1] = (tau) * (angle[1] + av_si[1] * dt) + (1.0 - tau) * accelRoll;
+    angle[2] = angle[2] + av_si[1] * dt;
 
-    drive_motor(0, IN1PH_PIN, left_speed);
-    drive_motor(1, IN2PH_PIN, right_speed);
+    error = angle[1] / 180.0 * PWM_MAX;
+    speed = c_p * error;
+    speed = speed > PWM_MAX ? (float)PWM_MAX : speed;
+    speed = speed < -PWM_MAX ? (float)(-PWM_MAX) : speed;
+    Serial.printf("speed = %d\n", (int16_t)speed);
+
+    drive_motor(0, IN1PH_PIN, speed);
+    drive_motor(1, IN2PH_PIN, -speed);
   }
-  delay(100);
 }
 
 void get_angle_from_accel(float aa_si[3], float *angle) {
@@ -156,7 +152,7 @@ void get_angle_from_accel(float aa_si[3], float *angle) {
   angle[2] = atanf(aa_si[2] / sqrtf(aa_si[1] * aa_si[1] + aa_si[0] * aa_si[0]));
 }
 
-void drive_motor(uint8_t channel, uint8_t pin, int8_t speed) {
+void drive_motor(uint8_t channel, uint8_t pin, int16_t speed) {
   if (speed > 0) {
     digitalWrite(pin, LOW);
   } else {
